6c3c39f4d2a3e7758bfb0e95c7704ffc8658db9f
Nadine Haninta <nadinehaninta@gmail.com>
2022-02-07 14:20:59 +0700

    #3 Edit home-logged in

--- application/views/Unused/home.php
+++ application/views/Unused/home.php
@@ -23,16 +23,16 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item active">
-          <a class="nav-link font-weight-bold" href="#" style="text-transform: capitalize; color: #383E56;">BERANDA</a>
+          <a class="nav-link font-weight-bold" href="#beranda" style="text-transform: capitalize; color: #383E56;">BERANDA</a>
         </li>
         <li class="nav-item active">
-          <a class="nav-link font-weight-bold" href="#" style="text-transform: capitalize; color: #383E56;">ACARA</a>
+          <a class="nav-link font-weight-bold" href="#acara" style="text-transform: capitalize; color: #383E56;">ACARA</a>
         </li>
         <li class="nav-item active">
-          <a class="nav-link font-weight-bold" href="#" style="text-transform: capitalize; color: #383E56;">MITRA</a>
+          <a class="nav-link font-weight-bold" href="#mitra" style="text-transform: capitalize; color: #383E56;">MITRA</a>
         </li>
         <li class="nav-item active">
-          <a class="nav-link font-weight-bold" href="#" style="text-transform: capitalize; color: #383E56; ;">SETELAN</a>
+          <a class="nav-link font-weight-bold" href="#setelan" style="text-transform: capitalize; color: #383E56; ;">SETELAN</a>
         </li>
       </ul>
       <form class="form-inline my-2 my-lg-0">
