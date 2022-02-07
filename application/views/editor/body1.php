84fe4a1071862617a9ba75ef7fc79af1f599e07b
Nadine Haninta <nadinehaninta@gmail.com>
2022-02-07 14:20:44 +0700

    #3 Edit home-logged in

--- application/views/editor/body1.php
+++ application/views/editor/body1.php
@@ -1,13 +1,4 @@
-<div class="main-banner wow fadeIn" id="top" data-wow-duration="1s" data-wow-delay="0.5s">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-12">
-          <div class="row">
-            <div class="col-lg-6 align-self-center">
-              <div class="left-content header-text wow fadeInLeft" data-wow-duration="1s" data-wow-delay="1s">
-                <div class="row">
-                  <div class="col-lg-4 col-sm-4">
-                    <div class="info-stat" id="buttons">
+<div class="col"
                       <h6>Reviewer Availability:</h6>
                       <h4>
                         <?php
